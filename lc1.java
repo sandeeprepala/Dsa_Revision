7bdb8e3b51be3c15ded3adc325f93257197d5196
sandeeprepala <sandeeprepala3@gmail.com>
2026-02-10 21:55:22 +0530

    Two Sum

---
+++ lc1.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class lc1 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                res[0] = map.get(target-nums[i]);
+                res[1] = i;
+            }else{
+                map.put(nums[i],i);
+            }
+        }
+        return res;
+    }
+}

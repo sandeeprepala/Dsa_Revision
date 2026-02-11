86a7342ef308918c259b891a6247b246780cc56c
sandeeprepala <sandeeprepala3@gmail.com>
2026-02-11 08:38:56 +0530

    Middle of a Linked List

---
+++ lc876.java
@@ -0,0 +1,11 @@
+public class lc876 {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}

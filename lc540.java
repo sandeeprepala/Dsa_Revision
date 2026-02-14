0a92a18e1c711388afe2f98f4bca1c601aeb62d9
sandeeprepala <sandeeprepala3@gmail.com>
2026-02-14 09:55:18 +0530

    Single Element in an Array

---
+++ lc540.java
@@ -0,0 +1,16 @@
+public class lc540 {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0, high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (mid % 2 == 1) mid--;
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+}

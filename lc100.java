1740289e057011b7eab5b4042b6f20412cb78f60
sandeeprepala <sandeeprepala3@gmail.com>
2026-02-18 23:21:41 +0530

    Same Tree

---
+++ lc100.java
@@ -0,0 +1,12 @@
+public class lc100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return helper(p,q);
+    }
+    public boolean helper(TreeNode p,TreeNode q){
+        
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        if(p.val != q.val) return false;
+        return helper(p.left,q.left) && helper(p.right,q.right);
+    }
+}

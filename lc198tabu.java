39a39709950f70845d9833a82a562710c6af7ef1
sandeeprepala <sandeeprepala3@gmail.com>
2026-02-23 23:53:34 +0530

    House Robber Tabulation

---
+++ lc198tabu.java
@@ -0,0 +1,26 @@
+public class lc198tabu {
+    public int rob(int[] nums) {
+    //     int n = nums.length;
+    //     int[] dp = new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //     return helper(nums,n-1,dp);
+    // }
+    // public int helper(int[]nums,int ind,int[]dp){
+    //     if(ind==0) return nums[ind];
+    //     if(ind<0) return 0;
+    //     if(dp[ind]!=-1) return dp[ind];
+    //     int dontTake = helper(nums,ind-1,dp);
+    //     int take = nums[ind]+helper(nums,ind-2,dp);
+    //     return dp[ind] = Math.max(dontTake,take);
+        int n = nums.length;
+        int[] dp = new int[n];
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max((dp[i-2]+nums[i]),dp[i-1]);
+        }
+        return dp[n-1];
+    }
+}

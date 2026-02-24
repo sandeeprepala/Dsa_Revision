cfe26d58e4da74164e315fca2ae8f5d193e095a5
sandeeprepala <sandeeprepala3@gmail.com>
2026-02-24 21:55:51 +0530

    Unique Paths II Memoization

---
+++ lc63memo.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class lc63memo {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        return helper(obstacleGrid,m-1,n-1,dp);
+    }
+    public int helper(int[][]obstacleGrid,int m,int n,int[][]dp){
+        if(m==0 && n==0 && obstacleGrid[m][n]==0) return 1;
+        if(m<0 || n<0) return 0;
+        if(obstacleGrid[m][n]==1) return 0;
+        if(dp[m][n]!=-1) return dp[m][n];
+        int left = helper(obstacleGrid,m-1,n,dp);
+        int top = helper(obstacleGrid,m,n-1,dp);
+        return dp[m][n] = left+top;
+    }
+}
